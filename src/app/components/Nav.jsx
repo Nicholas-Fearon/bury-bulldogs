8e807c06a75e04c7ce8f3a691786ee8e4a7b107c
Nicholas-Fearon <nejfearon@gmail.com>
2025-02-25 11:15:03 +0000

    Nav component implemented with Link routes to pages

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,5 +1,18 @@
+import Link from "next/link"
 export default function Nav() {
     return (<>
-        <p>About</p>
+            <Link href="/">Home</Link>
+
+        <Link href="/about">About Us</Link>
+        <Link href="/committee">Committee</Link>
+        <Link href="/policies">Policies</Link>
+        <Link href="/contact">Contact Us</Link>
+        <Link href="/teams">Our Teams</Link>
+        <Link href="/sponsorship">Sponsorship</Link>
+        <Link href="/news">News</Link>
+        <Link href="/shop">Shop</Link>
+
+
+
     </>)
 }
